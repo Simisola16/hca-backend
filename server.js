d88a7b35f839dc6f1bdd95170fc9878d61f28834
Muhayad olamilekan <olamilekanmuhayad@yahoo.com>
2026-03-07 19:45:19 +0100

    route

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const app = express();
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     allowedHeaders: ['Content-Type', 'Authorization']
 // }));
-app.use(cors("*"));
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
